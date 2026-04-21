ebc3f817db6436b9435004e59cf04dfbea9f8c9e
Ion-1 <ion1@skalamera.ch>
2026-04-21 11:01:51 +0200

    Update __main__.py

--- leeh_control/__main__.py
+++ leeh_control/__main__.py
@@ -7,7 +7,12 @@ from leeh_control.app import App
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--fake-backend", action="store_true")
+    parser.add_argument("-v", "--verbose", action="count", default=0, help="Increase verbosity")
+    parser.add_argument(
+        "-c", "--config", type=str, default=None, help="Path to the config file (default: config.toml)"
+    )
+    parser.add_argument("--fake-backend", action="store_true", help="Use fake backend")
+    parser.add_argument("-h", "--help", action="help", help="Show this help message and exit")
     namespace = parser.parse_args(sys.argv[1:])
 
     logging.captureWarnings(True)
