769c87361142e7a749786a9616757ce688ca7d0e
Ion-1 <ion1@skalamera.ch>
2026-04-22 16:04:10 +0200

    Add logging configuration

--- leeh_control/__main__.py
+++ leeh_control/__main__.py
@@ -6,17 +6,33 @@ import argparse
 from leeh_control.app import App
 
 
+def configure_logging(verbose_count: int) -> None:
+    level = logging.WARNING
+    if verbose_count >= 2 or __debug__:
+        level = logging.DEBUG
+    elif verbose_count == 1:
+        level = logging.INFO
+
+    logging.captureWarnings(True)
+    logging.basicConfig(level=level, format="[%(asctime)s] [%(name)s/%(levelname)s]: %(message)s")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--verbose", action="count", default=0, help="Increase verbosity")
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Increase verbosity (-v: INFO, -vv: DEBUG; default: WARNING)",
+    )
     parser.add_argument(
         "-c", "--config", type=str, default=None, help="Path to the config file (default: config.toml)"
     )
     parser.add_argument("--fake-backend", action="store_true", help="Use fake backend")
     namespace = parser.parse_args(sys.argv[1:])
 
-    logging.captureWarnings(True)
-    logging.basicConfig(level=logging.DEBUG)
+    configure_logging(namespace.verbose)
 
     if namespace.config is not None:
         try:
